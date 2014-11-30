c66056425fa53bcddbfef46147da9ffc0efc6894
unknown <kostaskostogloy@outlook.com>
2014-11-30 15:56:59 +0200

    Fix encoding

--- protected/views/site/_institution.php
+++ protected/views/site/_institution.php
@@ -6,7 +6,7 @@
             </div>
             <div class="col-lg-6">
                 <h3 style="margin-top:0;"><a href="<?= Yii::app()->createUrl('institution/view', array('id' => $data->id)) ?>"><?= $data->NAMEGRK; ?></a></h3>
-                <p><?= (strlen($data->abstract) > 200) ? mb_substr($data->abstract, 0, 200) . ' ...' : $data->abstract; ?></p>
+                <p><?= (strlen($data->abstract) > 200) ? mb_substr($data->abstract, 0, 200, 'utf-8') . ' ...' : $data->abstract; ?></p>
                 <p>
                     <span class="label label-primary"><?= $data->DIMOS ?></span>
                     <span class="label label-info"><?= $data->NEWCAT ?></span>
@@ -16,7 +16,7 @@
         </div>
     </div>
 </div>
-
+s
 <?php if ($index%2):?>
 <div class="clearfix"></div>
 </div><div class="row" style="margin-top: 30px !important;">
